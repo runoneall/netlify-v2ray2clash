5014077ccffb496a992f9aba5108cbf7bd0c196d
runoneall <runoobsteve@hotmail.com>
2025-01-30 02:32:52 +0800

    update

---
+++ func/clash.js
@@ -0,0 +1,11 @@
+import { ClashConfigBuilder } from './ClashConfigBuilder.js';
+
+export default async (request, context) => {
+    const inputString = await request.text();
+    let baseConfig;
+    const configBuilder = new ClashConfigBuilder(inputString, [], [], false, baseConfig);
+    const config = await configBuilder.build();
+    return new Response(config, {
+        headers: { 'Content-Type': 'text/yaml; charset=utf-8' },
+    });
+};
